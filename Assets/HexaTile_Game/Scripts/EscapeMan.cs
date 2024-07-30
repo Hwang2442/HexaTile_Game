c9a727759d47fe21ed44c432faaa94590134cd46
hwaaaang <68269611+Hwang2442@users.noreply.github.com>
2024-07-30 23:18:59 +0900

    Add Player Move

--- Assets/HexaTile_Game/Scripts/EscapeMan.cs
+++ Assets/HexaTile_Game/Scripts/EscapeMan.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using DG.Tweening;
+
 namespace HexaGridGame
 {
     public class EscapeMan : MonoBehaviour
     {
+        private Animator animator;
+
         public HexaTile Tile { get; set; }
 
         public bool Moving { get; private set; }
@@ -14,40 +18,24 @@ namespace HexaGridGame
 
         private void Start()
         {
-            //transform.position += Vector3.up * 0.2f;
-
+            animator = GetComponent<Animator>();
             Debug.Log("start");
+        }
+
+        private void OnDestroy()
+        {
+            transform.DOKill();
         }
 
         public void Move(HexaTile tile)
         {
             Moving = true;
+            animator.SetTrigger("Move");
+            transform.LookAt(tile.transform);
 
-            StartCoroutine(Co_Move(tile));
-        }
-
-        IEnumerator Co_Move(HexaTile tile)
-        {
-            float t = 0;
-
-            Vector3 start = transform.position;
-            Vector3 target = tile.transform.position;
-            target.y += 0.2f;
-
-            while (t < 1)
-            {
-                t += Time.deltaTime;
-
-                transform.position = Vector3.Lerp(start, target, t / 0.2f);
-
-                yield return null;
-            }
-
-            Tile = tile;
-
-            Moving = false;
-
-            Manager.PlayerOnClearTile(Tile);
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(transform.DOMove(tile.transform.position, 0.667f).SetEase(Ease.InOutSine));
+            sequence.AppendCallback(() => Moving = false);
         }
     }
 }
